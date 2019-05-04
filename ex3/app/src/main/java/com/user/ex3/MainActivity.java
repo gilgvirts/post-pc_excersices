7d0df744c54b3cbe4ef4c6d1211a49445fe11178
gilgvirts <gilgvirts@users.noreply.github.com>
2019-05-04 23:05:14 +0300

    v3 - added save

--- ex3/app/src/main/java/com/user/ex3/MainActivity.java
+++ ex3/app/src/main/java/com/user/ex3/MainActivity.java
@@ -1,6 +1,8 @@
 package com.user.ex3;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,13 +10,14 @@ import android.view.View;
 import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import java.util.ArrayList;
-import java.util.List;
 import android.support.v7.widget.LinearLayoutManager;
-import android.view.ViewGroup;
 import android.support.design.widget.*;
-
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import com.google.gson.Gson;
+import java.lang.reflect.Type;
+import com.google.gson.reflect.TypeToken;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, ChatUtils.MessageClickCallback {
     EditText textField;
@@ -22,6 +25,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     final int delete_message = 1;
     final String inp_id = "inp";
     final String chat_id = "disp";
+    static final String CHAT = "CHAT";
     final int delete_request_number = 666;
     private ChatUtils.MessageAdapter adapter
             = new ChatUtils.MessageAdapter();
@@ -31,7 +35,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        chat =  new ArrayList<>();
+        if (chat == null) {
+            chat = new ArrayList<Message>();
+        }
+        // load tasks from preference
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this.getApplicationContext());
+
+        try {
+            chat = getChat(CHAT);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         setContentView(R.layout.activity_main);
         textField = findViewById(R.id.input_insert);
         Button send = findViewById(R.id.input_send);
@@ -107,6 +121,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onSaveInstanceState(outState);
         outState.putString(inp_id,  textField.getText().toString());
         outState.putParcelableArrayList(chat_id, chat);
+        saveChat(chat, CHAT);
     }
-
+    public void saveChat(ArrayList<Message> list, String key){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this.getApplicationContext());
+        SharedPreferences.Editor editor = prefs.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(list);
+        editor.putString(key, json);
+        editor.apply();     // This line is IMPORTANT !!!
+    }
+    public ArrayList<Message> getChat(String key){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this.getApplicationContext());
+        Gson gson = new Gson();
+        String json = prefs.getString(key, null);
+        if (json != null){
+            Type type = new TypeToken<ArrayList<Message>>() {}.getType();
+            return gson.fromJson(json, type);
+        }
+        return new ArrayList<Message>();
+    }
 }
