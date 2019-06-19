ab52c820bbe61aa6869ffdbb5afb06ab20785222
gilgvirts <gilgvirts@users.noreply.github.com>
2019-06-20 01:28:14 +0300

    Update MainActivity.java

--- MySpecialStalker/app/src/main/java/com/example/myspecialstalker/MainActivity.java
+++ MySpecialStalker/app/src/main/java/com/example/myspecialstalker/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -13,32 +14,32 @@ import android.text.TextWatcher;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.security.Permission;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 
 @SuppressWarnings("deprecation")
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements TextWatcher{
     private static final String START_TEXT = "I'm going to call this number: ";
     private static final String PHONE_FIELD = "number_save";
     private static final String PREDEFINED_FIELD = "text_save";
     private static final String NOT_VALID_INPUT = "Missing information, please insert";
     private static final String APP_READY = "Thank you, stalking will start";
-    private static final int REQUEST_SMS_PERMISSION_CODE = 1546;
-    private static final int REQUEST_PHONE_PERMISSION_CODE = 1547;
 
     private TextView phoneField;
     private TextView preTextField;
     private TextView informerView;
+    private boolean permissionsGiven = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        askPermission(0);
         super.onCreate(savedInstanceState);
+        askPermission();
         setContentView(R.layout.activity_main);
         //try loading from SP
         loadFields();
-    }
 
+    }
 
     private void loadFields() {
         phoneField = findViewById(R.id.PhoneInsert);
@@ -48,41 +49,43 @@ public class MainActivity extends AppCompatActivity {
                 PreferenceManager.getDefaultSharedPreferences(this.getApplicationContext());
         phoneField.setText(prefs.getString(PHONE_FIELD, ""));
         preTextField.setText(prefs.getString(PREDEFINED_FIELD, START_TEXT));
-        phoneField.addTextChangedListener(new InputVerifier());
-        preTextField.addTextChangedListener(new InputVerifier());
+        phoneField.addTextChangedListener(this);
+        preTextField.addTextChangedListener(this);
 
 
     }
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String[] permissions, int[] grantResults) {
-        Log.d("Permissions", permissions + " results: " + Integer.toString(grantResults));
-        if (grantResults.length > 0
-                && !(grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-            switch (requestCode) {
-                case REQUEST_SMS_PERMISSION_CODE:
-                    askPermission(0);
-                case REQUEST_PHONE_PERMISSION_CODE:
-                    askPermission(1);
+//        Log.d("Permissions", permissions + " result);
+        if (grantResults.length > 0){
+            for (int res: grantResults){
+                if(res == PackageManager.PERMISSION_DENIED){
+                    askPermission();
+                }
             }
-        }
-        else if(requestCode == REQUEST_SMS_PERMISSION_CODE) {
-            askPermission(1);
         }
     }
 
-    private void askPermission(int permission) {
-        switch (permission) {
-            case 0:
-                ActivityCompat.requestPermissions(this, new String[]{
-                                Manifest.permission.SEND_SMS},
-                        REQUEST_SMS_PERMISSION_CODE);
-            case 1:
-                ActivityCompat.requestPermissions(this, new String[]{
-                                Manifest.permission.PROCESS_OUTGOING_CALLS},
-                        REQUEST_PHONE_PERMISSION_CODE);
+    private void askPermission() {
+        int PERMISSION_ALL = 1;
+        String[] PERMISSIONS = {
+                Manifest.permission.READ_PHONE_STATE
+        };
+
+        if (!hasPermissions(this, PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
         }
     }
+    private static boolean hasPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     @Override
@@ -95,7 +98,6 @@ public class MainActivity extends AppCompatActivity {
         editor.putString(PREDEFINED_FIELD, preTextField.getText().toString());
         editor.apply();
     }
-    class InputVerifier implements TextWatcher{
 
         @Override
         public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -103,13 +105,14 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            Log.d("Text changes", "preText: " + preTextField.getText() + " phoneField:" + phoneField.getText());
-            if(!phoneField.getText().equals("") && !preTextField.getText().equals("")){
-                informerView.setText(APP_READY);
-                //Start Broadcast
+            Log.d("Text changes", "preText: " + preTextField.getText() + " phoneField:" + phoneField.getText().length());
+            if(phoneField.getText().length() == 0 || (preTextField.getText().length() == 0)){
+                informerView.setText(NOT_VALID_INPUT);
+
             }
             else{
-                informerView.setText(NOT_VALID_INPUT);
+                //Start Broadcast
+                informerView.setText(APP_READY);
             }
         }
 
@@ -118,4 +121,4 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
-}
+//    Sending sms https://developer.android.com/guide/components/intents-common#java
